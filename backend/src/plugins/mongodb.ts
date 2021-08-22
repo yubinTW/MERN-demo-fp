a417cc204580c283ece0e50b8786e885ea669752
yubintw <yubin.cs05g@nctu.edu.tw>
2021-08-23 01:22:19 +0800

    refactor establishConnection() with fp-ts

--- backend/src/plugins/mongodb.ts
+++ backend/src/plugins/mongodb.ts
@@ -1,24 +1,39 @@
 import mongoose from 'mongoose'
 import * as dotEnv from 'dotenv'
+import { pipe } from 'fp-ts/function'
+import * as O from 'fp-ts/Option'
+import * as E from 'fp-ts/Either'
 
 dotEnv.config()
 const host = process.env.MONGO_HOST || 'localhost'
 const port = process.env.MONGO_PORT || 27017
 const database = process.env.MONGO_DATABASE || 'fastify'
 
-const establishConnection = () => {
-  if (!process.env.JEST_WORKER_ID && mongoose.connection.readyState === 0) {
-    mongoose.connect(
-      `mongodb://${host}:${port}/${database}`,
-      { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-      (err) => {
-        if (!err) console.log('MongoDB connection successful.')
-        else console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2))
-      }
-    )
-  } else {
-    console.log('MongoDB has connected.')
-  }
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 }
+
+const establishConnection = () =>
+  pipe(
+    !process.env.JEST_WORKER_ID && mongoose.connection.readyState === 0,
+    E.fromPredicate(
+      (b) => b,
+      () => console.log('Mongo had connected or running test cases')
+    ),
+    E.map((_) => {
+      mongoose.connect(`mongodb://${host}:${port}/${database}`, mongoOptions, (err) =>
+        pipe(
+          err,
+          O.fromNullable,
+          O.match(
+            () => console.log('MongoDB connection successful.'),
+            () => console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2))
+          )
+        )
+      )
+    })
+  )
 
 export { establishConnection }
